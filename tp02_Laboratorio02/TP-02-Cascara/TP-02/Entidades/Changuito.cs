816094a90db7743d7b3b8d0d4da92fad3c3a5b31
alumno <alumno@utnfra.com.ar>
2019-05-21 19:52:09 -0300

    RECUPERATORIO

--- tp02_Laboratorio02/TP-02-Cascara/TP-02/Entidades/Changuito.cs
+++ tp02_Laboratorio02/TP-02-Cascara/TP-02/Entidades/Changuito.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,123 +7,123 @@ using Entidades;
 
 namespace Entidades
 {
-    /// <summary>
-    /// No podrá tener clases heredadas.
-    /// </summary>
-    public class Changuito
+  /// <summary>
+  /// No podrá tener clases heredadas.
+  /// </summary>
+  public class Changuito
+  {
+    List<Producto> productos;
+    private int espacioDisponible;
+
+    public enum ETipo
     {
-        List<Producto> productos;
-        private int espacioDisponible;
+      Dulce, Leche, Snacks, Todos
+    }
 
-        public enum ETipo
+    private Changuito()
+    {
+      this.productos = new List<Producto>();
+    }
+
+    public Changuito(int espacioDisponible) : this()
+    {
+      this.espacioDisponible = espacioDisponible;
+    }
+
+    /// <summary>
+    /// Agrega un Producto al Changuito
+    /// </summary>
+    /// <param name="c"></param>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    public static Changuito operator +(Changuito c, Producto p)
+    {
+      int cantidad;
+      cantidad = c.productos.Count();
+      foreach (Producto pAux in c.productos)
+      {
+        if (p == pAux)
         {
-            Dulce, Leche, Snacks, Todos
+          return c;
         }
+      }
+      if (cantidad < c.espacioDisponible)
+      {
+        c.productos.Add(p);
+      }
+      return c;
+    }
 
-		private Changuito() 
-		{
-			this.productos = new List<Producto>(); 
-		}
+    /// <summary>
+    /// Elimina un producto del Chamguito
+    /// </summary>
+    /// <param name="c"></param>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    public static Changuito operator -(Changuito c, Producto p)
+    {
+      foreach (Producto pAux in c.productos)
+      {
+        if (p == pAux)
+        {
+          c.productos.Remove(p);
+          break;
+        }
+      }
+      return c;
+    }
 
-		public Changuito(int espacioDisponible) : this()
-		{
-			this.espacioDisponible = espacioDisponible;
-		}
+    /// <summary>
+    /// Sobreescritura, muestra la cantidad de productos y el espacio disponible
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+      StringBuilder sb = new StringBuilder();
+      sb.AppendLine("Tenemos " + this.productos.Count() + " lugares ocupados de un total de " +
+          espacioDisponible + " disponibles");
+      return sb.ToString();
+    }
 
-		/// <summary>
-		/// Agrega un Producto al Changuito
-		/// </summary>
-		/// <param name="c"></param>
-		/// <param name="p"></param>
-		/// <returns></returns>
-		public static Changuito operator +(Changuito c, Producto p)
-		{
-			int cantidad;
-			cantidad = c.productos.Count();
-			foreach (Producto pAux in c.productos)
-			{
-				if (p == pAux )
-				{
-					return c;
-				}
-			}
-			if(cantidad < c.espacioDisponible)
-			{
-				c.productos.Add(p);
-			}
-			return c;
-		}
+    /// <summary>
+    /// Muestra Producto dependiendo de su Tipo
+    /// </summary>
+    /// <param name="c"></param>
+    /// <param name="tipo"></param>
+    /// <returns></returns>
+    public string Mostrar(Changuito c, ETipo tipo)
+    {
+      StringBuilder sb = new StringBuilder(ToString());
+      foreach (Producto p in productos)
+      {
+        switch (tipo)
+        {
+          case ETipo.Dulce:
+            if (p is Dulce)
+            {
+              sb.Append(p.Mostrar());
+            }
+            break;
+          case ETipo.Leche:
+            if (p is Leche)
+            {
+              sb.Append(p.Mostrar());
+            }
+            break;
+          case ETipo.Snacks:
+            if (p is Snacks)
+            {
+              sb.Append(p.Mostrar());
+            }
+            break;
+          default:
+            sb.Append(p.Mostrar());
+            break;
 
-		/// <summary>
-		/// Elimina un producto del Chamguito
-		/// </summary>
-		/// <param name="c"></param>
-		/// <param name="p"></param>
-		/// <returns></returns>
-		public static Changuito operator -(Changuito c, Producto p)
-		{
-			foreach (Producto pAux in c.productos)
-			{
-				if(p == pAux)
-				{
-					c.productos.Remove(p);
-					break;
-				}
-			}
-			return c;
-		}
+        }
+      }
+      return sb.ToString();
 
-		/// <summary>
-		/// Sobreescritura, muestra la cantidad de productos y el espacio disponible
-		/// </summary>
-		/// <returns></returns>
-		public override string ToString()
-		{
-			StringBuilder sb = new StringBuilder();
-			sb.AppendLine("Tenemos " + this.productos.Count() + " lugares ocupados de un total de " +
-				espacioDisponible + " disponibles");
-			return sb.ToString();	
-		}
-
-		/// <summary>
-		/// Muestra Producto dependiendo de su Tipo
-		/// </summary>
-		/// <param name="c"></param>
-		/// <param name="tipo"></param>
-		/// <returns></returns>
-		public string Mostrar(Changuito c, ETipo tipo)
-		{
-			StringBuilder sb = new StringBuilder(ToString());
-			foreach(Producto p in productos)
-			{
-				switch (tipo)
-				{
-					case ETipo.Dulce:
-						if(p is Dulce)
-						{
-							sb.Append(p.Mostrar());
-						}
-						break;
-					case ETipo.Leche:
-						if (p is Leche)
-						{
-							sb.Append(p.Mostrar());
-						}
-						break;
-					case ETipo.Snacks:
-						if (p is Snacks)
-						{
-							sb.Append(p.Mostrar());
-						}
-						break;
-					default:
-						sb.Append(p.Mostrar());
-						break;
-
-				}
-			}
-			return sb.ToString();
-
-		}
-	}
+    }
+  }
 }
