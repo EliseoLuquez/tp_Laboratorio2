004c4c47f1a9ef3f8e6473bdd9854727947d0edc
alumno <alumno@utnfra.com.ar>
2019-05-21 22:01:07 -0300

    Terminado Recuperatorio

--- tp02_Laboratorio02/TP-02-Cascara/TP-02/Entidades/Dulce.cs
+++ tp02_Laboratorio02/TP-02-Cascara/TP-02/Entidades/Dulce.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,37 +6,37 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-  public class Dulce : Producto
-  {
-    public Dulce(EMarca marca, string patente, ConsoleColor color) : base(patente, marca, color)
+    public class Dulce : Producto
     {
+        public Dulce(EMarca marca, string patente, ConsoleColor color) : base(patente, marca, color)
+        {
 
+        }
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get { return  80;  }
+        }
+
+
+		/// <summary>
+		/// Muestra los atributos de Producto y Dulce
+		/// </summary>
+		/// <returns></returns>
+		public override  string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE ");
+			sb.AppendLine((string)this);
+			sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
     }
-
-    /// <summary>
-    /// Los dulces tienen 80 calorías
-    /// </summary>
-    protected short CantidadCalorias
-    {
-      get { return 80; }
-    }
-
-
-    /// <summary>
-    /// Muestra los atributos de Producto y Dulce
-    /// </summary>
-    /// <returns></returns>
-    public override string Mostrar()
-    {
-      StringBuilder sb = new StringBuilder();
-
-      sb.AppendLine("DULCE ");
-      sb.AppendLine((string)this);
-      sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
-      sb.AppendLine("");
-      sb.AppendLine("---------------------");
-
-      return sb.ToString();
-    }
-  }
 }
